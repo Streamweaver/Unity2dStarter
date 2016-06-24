d928318fab1aae66e640c40f13c4ea61277bcf69
Scott Turnbull <streamweaver@gmail.com>
2016-06-23 21:22:55 -0400

    Initial Commit

---
+++ Assets/Scripts/GUIController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GUIController : MonoBehaviour {
+
+	[SerializeField] private MenuPopup menuPopup;
+
+	void Start() {
+		menuPopup.Close ();
+	}
+
+	public void OnMainMenuClick () {
+		if (menuPopup.IsActive ()) {
+			menuPopup.Close ();
+		} else {
+			menuPopup.Open ();
+		}
+	}
+
+	public void QuitRequest() {
+		Application.Quit ();
+	}
+
+	public void LoadLevel(string name) {
+		SceneManager.LoadScene (name);
+	}
+}
